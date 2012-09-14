23917dfaf84949432e5c990501cf96e44044b26c
tefino <sjsssd@126.com>
2012-09-14 11:08:27 +0800

    Update deployment/startserver.cpp

--- deployment/startserver.cpp
+++ deployment/startserver.cpp
@@ -6,6 +6,6 @@ int main(int argc, char* argv[])
 {
 	char cmd[100] ;
 	int i = atoi(argv[1]) ;
-	sprintf(cmd, "ssh root@172.16.20.%d \"/home/publisher_opc 1000 20 10 > /tmp/publisher_output_opc.debug 2>&1 &\"", i) ;
+	sprintf(cmd, "ssh root@172.16.20.%d \"/home/publisher_opc 1000 20 1 > /tmp/publisher_output_opc.debug 2>&1 &\"", i) ;
 	system(cmd) ;
 }
