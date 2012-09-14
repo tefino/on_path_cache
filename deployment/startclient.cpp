6d565598f6a9bad4b03d0ecdae2cca3c4dd23e05
tefino <sjsssd@126.com>
2012-09-14 11:08:51 +0800

    Update deployment/startclient.cpp

--- deployment/startclient.cpp
+++ deployment/startclient.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* arg[])
 
 	for(int i = 71 ; i <= 75 ; i++)
 	{
-		sprintf(cmd, "ssh root@172.16.20.%d \"/home/subscriber_opc 1000 20 10 100 1 120 > /tmp/client_output_opc.debug 2>&1 &\"", i) ;
+		sprintf(cmd, "ssh root@172.16.20.%d \"/home/subscriber_opc 1000 20 1 0 1 1800 > /tmp/client_output_opc.debug 2>&1 &\"", i) ;
 		system(cmd) ;
 	}
 }
